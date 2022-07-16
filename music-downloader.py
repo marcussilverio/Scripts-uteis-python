b9ee4bb36e1ed8529920dca81a7d006a79e09ccf
Marcus Silverio <marcus_vs@live.com>
2022-07-16 14:09:57 -0300

    changed the output file extension from .mp4 to .mp3

--- music-downloader.py
+++ music-downloader.py
@@ -1,4 +1,10 @@
 import pytube
+import os
 url = input("Enter video url: ")
 path = 'D:\Musicas'
-pytube.YouTube(url).streams.get_audio_only().download(path)
+yt = pytube.YouTube(url)
+out_file = yt.streams.get_audio_only().download(path)
+base, ext = os.path.splitext(out_file)
+new_file = base +'.mp3'
+os.rename(out_file, new_file)
+print(yt.title + " has been successfully downloaded.")
